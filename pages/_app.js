2023b304262fe8bd9d0977f7bb9f1a424108fc79
Qxtlp <xi.tulop@yandex.com>
2022-05-29 20:36:55 +0700

    redux setup

--- pages/_app.js
+++ pages/_app.js
@@ -4,13 +4,24 @@ import "../styles/main.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import { useEffect } from "react";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import stores from "stores";
+
+const { store, persistor } = stores;
 
 function MyApp({ Component, pageProps }) {
   useEffect(() => {
     import("bootstrap/dist/js/bootstrap");
   }, []);
 
-  return <Component {...pageProps} />;
+  return (
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <Component {...pageProps} />
+      </PersistGate>
+    </Provider>
+  );
 }
 
 export default MyApp;
