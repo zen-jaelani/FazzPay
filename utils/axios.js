6124cf079cd033afe04cbe6a3fa9d088211151ed
Qxtlp <xi.tulop@yandex.com>
2022-06-01 23:07:41 +0700

    add error check

--- utils/axios.js
+++ utils/axios.js
@@ -20,4 +20,22 @@ axiosApiIntances.interceptors.request.use(
   }
 );
 
+axiosApiIntances.interceptors.response.use(
+  function (response) {
+    // Any status code that lie within the range of 2xx cause this function to trigger
+    // Do something with response data
+    return response;
+  },
+  function (error) {
+    // Any status codes that falls outside the range of 2xx cause this function to trigger
+    // Do something with response error
+    if (error.response.status === 403) {
+      localStorage.clear();
+      Cookies.remove("token");
+      Cookies.remove("id");
+      window.location.href = "/auth/login";
+    }
+    return Promise.reject(error);
+  }
+);
 export default axiosApiIntances;
