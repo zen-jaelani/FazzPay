c6db77a7cf385586b62ee7f4ccb4c90f618a06ea
Qxtlp <xi.tulop@yandex.com>
2022-06-01 22:58:51 +0700

    fix some ui

--- components/Layout/auth.jsx
+++ components/Layout/auth.jsx
@@ -8,14 +8,17 @@ export default function MainLayout(props) {
       <Head>
         <title>{props.head}</title>
       </Head>
-      <div className="bgSecondary " style={{ minHeight: "100vh" }}>
-        <div className="container rounder-top shadow-lg m-0 p-0 mx-auto my-lg-5 row overflow-hidden bg-white">
+      <div className="bgSecondary" style={{ maxHeight: "100vh" }}>
+        <div
+          className="container rounder-top shadow-lg m-0 p-0 mx-auto my-lg-5 row overflow-hidden bg-white"
+          style={{ maxHeight: "inherit" }}
+        >
           <div className="col-12 col-lg-6 p-0">
             <Banner />
           </div>
           <div
-            className="col-12 col-lg-6 authMain bg-white h-100"
-            style={{ minHeight: "75vh" }}
+            className="col-12 col-lg-6 authMain bg-white"
+            style={{ maxHeight: "inherit" }}
           >
             <div className="row ps-2 ps-lg-5">
               <div className="col-12 text-center text-lg-start col-lg-9 mt-3">
@@ -29,7 +32,7 @@ export default function MainLayout(props) {
                 <h6 className="d-none d-lg-block text-black-50 mt-4 lh-lg">
                   {props.dSubTitle}
                 </h6>
-                {props.children}
+                <div>{props.children}</div>
               </div>
             </div>
           </div>
