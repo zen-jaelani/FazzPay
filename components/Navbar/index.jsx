c35779dbb1d8213a8eb0d80bda2d5826c9eceb3b
Qxtlp <xi.tulop@yandex.com>
2022-06-01 22:59:36 +0700

    navbar integration

--- components/Navbar/index.jsx
+++ components/Navbar/index.jsx
@@ -1,18 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import Image from "next/image";
+import { useDispatch } from "react-redux";
+import { getUser } from "stores/action/user";
+import Cookies from "js-cookie";
+import { Dropdown, DropdownButton } from "react-bootstrap";
+import { histoy } from "stores/action/transaction";
 
 export default function Navbar() {
   const router = useRouter();
+  const dispatch = useDispatch();
+  const [user, setUser] = useState({});
+  const [dataHistory, setDataHistory] = useState([]);
+
+  useEffect(() => {
+    dispatch(getUser(Cookies.get("id")))
+      .then((result) => {
+        setUser(result.value.data.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    dispatch(histoy({ page: 1, limit: 4, filter: "" }))
+      .then((res) => setDataHistory(res.value.data.data))
+      .catch((err) => console.log(err));
+  }, []);
+
+  console.log(dataHistory);
   const handleLogout = () => {
     router.push("/login");
   };
   return (
     <div>
-      {/* <Link href="/home">home</Link>
-      <button onClick={handleLogout}>Logout</button> */}
-
       <div className="bg-white shadow-sm w-100 container-fluid py-4">
         <div className="container d-flex justify-content-between">
           <h3 className="textMain fw-bold mt-3">Zwallet</h3>
@@ -21,14 +41,53 @@ export default function Navbar() {
               className="col-3 ms-2 "
               style={{ height: "52px", width: "52px", position: "relative" }}
             >
-              <Image src="/Group 57.png" alt="" layout="fill" />
+              <Image
+                src={
+                  user.image
+                    ? `${process.env.IMAGE_URL}${user.image}`
+                    : "https://res.cloudinary.com/qxtlp/image/upload/v1651069637/default-profile.jpg"
+                }
+                layout="fill"
+                alt=""
+                className="visible"
+              ></Image>
             </div>
             <div className="col-6 col-lg-6 ">
-              <h3 className="fw-bold">Name</h3>
-              <p className="mb-1 fw-light">phoen</p>
+              <h6 className="fw-bold">
+                {user?.firstName} {user?.lastName}
+              </h6>
+              <p className="mb-1 fw-light">{user?.noTelp || "+00000000"}</p>
             </div>
-            <div className="col-1 ms-3">
-              <i className="bi bi-bell fs-3 d-block"></i>
+            <div className="col-1">
+              <DropdownButton
+                variant=""
+                title={<i className="bi bi-bell fs-4 d-block"></i>}
+                id="input-group-dropdown-1"
+              >
+                {dataHistory
+                  ? dataHistory.map((v) => (
+                      <Dropdown.Item key={v.id}>
+                        <div className="shadow-sm p-3 rounder d-flex">
+                          {v.type == "send" ? (
+                            <i className="bi bi-arrow-up me-4 fs-2 text-danger"></i>
+                          ) : (
+                            <i className="bi bi-arrow-down me-4 fs-2 text-success"></i>
+                          )}
+                          <div>
+                            {v.type == "send" ? (
+                              <span>Tranfer to {v.fullName} </span>
+                            ) : v.type == "accept" ? (
+                              <span>Accept from {v.fullName} </span>
+                            ) : (
+                              <span>Topup </span>
+                            )}
+                            <h5 className="fw-bold">Rp.12312</h5>
+                          </div>
+                        </div>
+                      </Dropdown.Item>
+                    ))
+                  : ""}
+              </DropdownButton>
             </div>
           </div>
         </div>
